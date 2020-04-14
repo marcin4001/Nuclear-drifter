64d0fe31ac87ae5c6f90ddf503420a434dd357ff
marcin4001 <mjachymczyk1@gmail.com>
2020-04-14 16:32:42 +0200

    Time and day cycle

---
+++ Nuclear-drifter/Assets/Scripts/TimeGame/DayCycle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    public Transform sun;
+    public Light lightPlayer;
+    private TimeGame time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = FindObjectOfType<TimeGame>();
+        lightPlayer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time.hour == 19)
+        {
+            float alfa = (float) time.minutes / 59f;
+            float x = Mathf.Lerp(0f, 90f, alfa);
+            Debug.Log(alfa);
+            sun.transform.rotation = Quaternion.Euler(new Vector3(x, 0f, 0f));
+            if (time.minutes > 40) lightPlayer.enabled = true;
+        }
+
+        if(time.hour == 4)
+        {
+            float alfa = (float)time.minutes / 59f;
+            float x = Mathf.Lerp(90f, 0f, alfa);
+            sun.rotation = Quaternion.Euler(new Vector3(x, 0f, 0f));
+            if (time.minutes > 25) lightPlayer.enabled = false;
+        }
+    }
+}
---
+++ Nuclear-drifter/Assets/Scripts/TimeGame/TimeGame.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeGame : MonoBehaviour
+{
+    public int day = 1;
+    public int hour = 5;
+    public int minutes = 0;
+
+    public float counter = 0.0f;
+    public float counterMax = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter >= counterMax)
+        {
+            if(minutes >= 59)
+            {
+                if(hour >= 23)
+                {
+                    day++;
+                    hour = 0;
+                }
+                else hour++;
+                minutes = 0;
+            }
+            else
+            {
+                minutes++;
+            }
+            counter = 0f;
+        }
+        else
+        {
+            counter += Time.deltaTime;
+        }
+    }
+
+    private void OnGUI()
+    {
+        string time = "Day: " + day + " Time: " + string.Format("{0:00}", hour) + ":" + string.Format("{0:00}", minutes);
+        GUI.Box(new Rect(10, 10, 150, 25),time);
+    }
+}
