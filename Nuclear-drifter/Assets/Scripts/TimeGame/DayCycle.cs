bbb15465a8722f700947a5763fbdbb5272a37351
marcin4001 <mjachymczyk1@gmail.com>
2020-04-15 23:48:46 +0200

    A second settlement was added

--- Nuclear-drifter/Assets/Scripts/TimeGame/DayCycle.cs
+++ Nuclear-drifter/Assets/Scripts/TimeGame/DayCycle.cs
@@ -7,6 +7,7 @@ public class DayCycle : MonoBehaviour
     public Transform sun;
     public Light lightPlayer;
     private TimeGame time;
+    public float counter = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +18,23 @@ public class DayCycle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(time.hour == 19)
+        if (time.hour == 19)
         {
-            float alfa = (float) time.minutes / 59f;
+            counter += Time.deltaTime;
+            float alfa = counter / 30f; //(float) time.minutes / 59f;
             float x = Mathf.Lerp(0f, 90f, alfa);
             Debug.Log(alfa);
             sun.transform.rotation = Quaternion.Euler(new Vector3(x, 0f, 0f));
             if (time.minutes > 40) lightPlayer.enabled = true;
         }
-
-        if(time.hour == 4)
+        else if (time.hour == 4)
         {
-            float alfa = (float)time.minutes / 59f;
+            counter += Time.deltaTime;
+            float alfa = counter / 30f; //(float) time.minutes / 59f;
             float x = Mathf.Lerp(90f, 0f, alfa);
             sun.rotation = Quaternion.Euler(new Vector3(x, 0f, 0f));
             if (time.minutes > 25) lightPlayer.enabled = false;
         }
+        else counter = 0.0f;
     }
 }
