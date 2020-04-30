d7a7cc7abb6db20aa913db51008fe770893c190f
marcin4001 <mjachymczyk1@gmail.com>
2020-04-30 23:20:32 +0200

    Watown Interior 2

--- Nuclear-drifter/Assets/Scripts/Teleport/Carpet.cs
+++ Nuclear-drifter/Assets/Scripts/Teleport/Carpet.cs
@@ -8,20 +8,30 @@ public class Carpet : MonoBehaviour
     public Animator door;
     public string sceneName;
     public Vector2 startPos;
+    private GUIScript gUI;
+    public bool isLock = false;
     // Start is called before the first frame update
     void Start()
     {
         box = FindObjectOfType<MessageBox>();
+        gUI = FindObjectOfType<GUIScript>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Hero")
         {
-            if (door != null) box.door = door;
-            box.sceneName = sceneName;
-            box.playerPos = startPos;
-            box.ShowBox();
+            if (!isLock)
+            {
+                if (door != null) box.door = door;
+                box.sceneName = sceneName;
+                box.playerPos = startPos;
+                box.ShowBox();
+            }
+            else
+            {
+                gUI.AddText("The door is locked!");
+            }
         }
     }
 }
