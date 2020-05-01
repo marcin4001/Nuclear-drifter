7d3c6565a034956eaa86d392dcc1ade47a4efc99
marcin4001 <mjachymczyk1@gmail.com>
2020-05-01 15:53:36 +0200

    First version world map

---
+++ Nuclear-drifter/Assets/Scripts/Map/MapControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapControl : MonoBehaviour
+{
+    public Canvas mapCanvas;
+    public Camera mapCam;
+    private bool actveMap = false;
+    private GUIScript gUI;
+    private PlayerClickMove player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapCanvas.enabled = false;
+        mapCam.enabled = false;
+        gUI = FindObjectOfType<GUIScript>();
+        player = FindObjectOfType<PlayerClickMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            actveMap = !actveMap;
+            mapCam.enabled = actveMap;
+            mapCanvas.enabled = actveMap;
+            gUI.blockGUI = actveMap;
+            player.active = !actveMap;
+        }
+    }
+}
---
+++ Nuclear-drifter/Assets/Scripts/Map/MapCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCursor : MonoBehaviour
+{
+    public Transform cursorPlayer;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cursorPlayer.localPosition = player.position;
+    }
+}
