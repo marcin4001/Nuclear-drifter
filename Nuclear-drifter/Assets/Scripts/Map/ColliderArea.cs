0835e0e1084872b81bfefaa30e8479cf4c867dc8
marcin4001 <mjachymczyk1@gmail.com>
2020-05-02 18:40:54 +0200

    Add name areas to map

---
+++ Nuclear-drifter/Assets/Scripts/Map/ColliderArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderArea : MonoBehaviour
+{
+    private SignController controller;
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = FindObjectOfType<SignController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.tag == "Hero")
+        {
+            controller.FoundArea(index);
+        }
+
+    }
+}
---
+++ Nuclear-drifter/Assets/Scripts/Map/SignController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class Area
+{
+    public TextMeshPro text;
+    public GameObject col;
+
+    public void ActiveText()
+    {
+        text.enabled = true;
+        GameObject.Destroy(col);
+    }
+
+    public void DeactiveText()
+    {
+        text.enabled = false;
+    }
+}
+
+
+public class SignController : MonoBehaviour
+{
+    public Area[] areas;
+    private GUIScript gUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gUI = FindObjectOfType<GUIScript>();
+        if(areas.Length > 0)
+        {
+            for(int i = 0; i < areas.Length; i++)
+            {
+                if (PropertyPlayer.property.foundArea[i])
+                    areas[i].ActiveText();
+                else
+                    areas[i].DeactiveText();
+            }
+        }
+    }
+
+    public void FoundArea(int index)
+    {
+        if (index >= 0 && index < areas.Length)
+        {
+            areas[index].ActiveText();
+            gUI.AddText("You found " + areas[index].text.text);
+            PropertyPlayer.property.foundArea[index] = true;
+        }
+    }
+}
