7904a268ad5fc21c97c24d6034ca096e7bed255e
marcin4001 <mjachymczyk1@gmail.com>
2020-05-05 15:49:33 +0200

    Add Nuke Bomb and First Radiation Area

---
+++ Nuclear-drifter/Assets/Scripts/Player stats/RadTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadTrigger : MonoBehaviour
+{
+    public Health health;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        health = collision.GetComponentInParent<Health>();
+        if (health != null) health.isRad = true;
+    }
+}
