aa2c8fcd9a93410b34e80189580bcfafcbcf1431
marcin4001 <mjachymczyk1@gmail.com>
2020-05-27 23:03:16 +0200

    Add Item grid and Item slot

---
+++ Nuclear-drifter/Assets/Scripts/Items/Slot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Slot
+{
+    public Item itemElement;
+    public int amountItem = 0;
+    public int ammo = 0;
+
+    public string GetAmount()
+    {
+        if(amountItem >= 1000)
+        {
+            int amount = amountItem / 1000;
+            return amount + "K";
+        }
+        if (amountItem == 1) return "";
+        return amountItem + "";
+    }
+
+    public string GetAmmo()
+    {
+        if (ammo >= 1000)
+        {
+            int amount = ammo / 1000;
+            return amount + "k";
+        }
+        return ammo + "";
+    }
+}
---
+++ Nuclear-drifter/Assets/Scripts/Items/SlotElement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotElement : MonoBehaviour
+{
+    public Slot itemSlot;
+    public Text labelSlot;
+    public Image imgSlot;
+    private GUIScript gUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gUI = FindObjectOfType<GUIScript>();
+        if(itemSlot.itemElement == null || itemSlot.amountItem <= 0)
+        {
+            ClearSlot();
+        }
+        else
+        {
+            imgSlot.enabled = true;
+            imgSlot.sprite = itemSlot.itemElement.image;
+            labelSlot.text = itemSlot.GetAmount();
+        }
+    }
+
+    public void ClickSlot()
+    {
+        if (itemSlot != null)
+        {
+            if (itemSlot.itemElement != null && itemSlot.amountItem > 0)
+            {
+                itemSlot.itemElement.Look();
+                if (itemSlot.itemElement.GetItemType() == ItemType.Weapon)
+                {
+                    WeaponItem weapon = (WeaponItem)itemSlot.itemElement;
+                    if (!weapon.isMeleeWeapon) gUI.AddText("Ammo amount: " + itemSlot.ammo);
+                }
+                gUI.AddText("Amount: " + itemSlot.amountItem);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I) && itemSlot != null)
+        {
+            if (itemSlot.itemElement != null && itemSlot.amountItem > 0)
+            {
+
+                imgSlot.enabled = true;
+                imgSlot.overrideSprite = itemSlot.itemElement.image;
+                labelSlot.text = itemSlot.GetAmount();
+            }
+        }
+    }
+
+    public void SetSlot(Slot newSlot)
+    {
+        itemSlot = newSlot;
+        imgSlot.enabled = true;
+        imgSlot.overrideSprite = itemSlot.itemElement.image;
+        labelSlot.text = itemSlot.GetAmount();
+    }
+
+    public void ClearSlot()
+    {
+        itemSlot = null;
+        imgSlot.enabled = false;
+        labelSlot.text = "";
+    }
+}
