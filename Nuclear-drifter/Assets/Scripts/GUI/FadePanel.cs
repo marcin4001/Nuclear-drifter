cd39f34b3c0aacc178fdee5d65141a41789b30d0
marcin4001 <mjachymczyk1@gmail.com>
2020-06-03 16:54:34 +0200

    Adding Bed System to other scenes

--- Nuclear-drifter/Assets/Scripts/GUI/FadePanel.cs
+++ Nuclear-drifter/Assets/Scripts/GUI/FadePanel.cs
@@ -9,21 +9,27 @@ public class FadePanel : MonoBehaviour
     private Animator anim;
     private string nameMethod = "";
     private GameObject objMethod;
+    private PauseMenu menu;
+    private MapControl map;
     // Start is called before the first frame update
     void Start()
     {
         img = GetComponent<Image>();
         img.enabled = false;
         anim = GetComponent<Animator>();
+        menu = FindObjectOfType<PauseMenu>();
+        map = FindObjectOfType<MapControl>();
     }
 
     public void Fade(string method, GameObject obj)
     {
         objMethod = obj;
         nameMethod = method;
-
+        
         anim.SetTrigger("FadeIn");
         Invoke("FadeOut", 1.4f);
+        menu.activeEsc = false;
+        map.keyActive = false;
     }
 
     //private void Update()
@@ -38,6 +44,13 @@ public class FadePanel : MonoBehaviour
     {
         objMethod.SendMessage(nameMethod);
         anim.SetTrigger("FadeOut");
+        Invoke("Config", 2.2f);
+    }
+
+    private void Config()
+    {
+        menu.activeEsc = true;
+        map.keyActive = true;
     }
 
     //private void Test()
