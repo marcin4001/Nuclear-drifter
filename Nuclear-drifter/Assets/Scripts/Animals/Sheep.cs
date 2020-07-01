29906c2761587565e32d7101c03c968af1a32222
marcin4001 <mjachymczyk1@gmail.com>
2020-07-01 23:31:58 +0200

    Adding Sheeps and sheep controller

---
+++ Nuclear-drifter/Assets/Scripts/Animals/Sheep.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public bool initState = false;
+    private bool isEating;
+    private Animator anim;
+    private string propsIE = "isEating";
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEating = initState;
+        anim = GetComponent<Animator>();
+        anim.SetBool(propsIE, isEating);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.J))
+        //{
+        //    ChangeState();
+        //}
+    }
+
+    public void ChangeState()
+    {
+        isEating = !isEating;
+        anim.SetBool(propsIE, isEating);
+    }
+}
---
+++ Nuclear-drifter/Assets/Scripts/Animals/SheepController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour
+{
+    public Sheep[] sheeps;
+    public int prefIndex = -1;
+    public int currentIndex = -1;
+
+    public float counter = 0;
+    public float counterMax = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sheeps = GetComponentsInChildren<Sheep>();
+        prefIndex = -1;
+        currentIndex = -1;
+        counter = 0;
+        counterMax = Random.Range(3, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter < counterMax)
+        {
+            counter += Time.deltaTime;
+        }
+        else
+        {
+            currentIndex = Random.Range(0, sheeps.Length);
+            if(currentIndex != prefIndex)
+            {
+                prefIndex = currentIndex;
+                counterMax = Random.Range(3, 6);
+                sheeps[currentIndex].ChangeState();
+                counter = 0;
+            }
+        }
+    }
+}
