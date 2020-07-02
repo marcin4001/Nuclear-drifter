9b9cdf0d6b5a48c4f60859f6d8e4683f1194dc05
marcin4001 <mjachymczyk1@gmail.com>
2020-07-02 15:19:28 +0200

    Addition of sheep to other settlements

---
+++ Nuclear-drifter/Assets/Scripts/Interactions/MultilineObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultilineObstacle : MonoBehaviour
+{
+    public string[] texts;
+    // Start is called before the first frame update
+    private GUIScript gUI;
+    public void ShowText()
+    {
+        if (gUI == null) gUI = FindObjectOfType<GUIScript>();
+        
+        if (gUI != null) {
+            if (texts.Length >= 2) gUI.ClearText();
+            foreach (string t in texts) gUI.AddText(t);
+        }
+        //Debug.Log(textItem);
+    }
+}
