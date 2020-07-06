50f42491fe840b63830f687cd6cb6417635cec8a
marcin4001 <mjachymczyk1@gmail.com>
2020-07-06 18:28:01 +0200

    Added death scene and animation

---
+++ Nuclear-drifter/Assets/Scripts/GUI/DeathScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    public bool active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+        Invoke("SetActive", 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(active && Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void SetActive()
+    {
+        active = true;
+    }
+}
