5d3cdb1fa044b88e3e9497c86eaf386d6dc72863
marcin4001 <mjachymczyk1@gmail.com>
2020-07-27 17:55:55 +0200

    new Item and item replace component

---
+++ Nuclear-drifter/Assets/Scripts/Items/ItemReplace.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemReplace : MonoBehaviour
+{
+    public Item itemReplace;
+    public Slot currentItem;
+    public bool isReplace = false;
+    public int indexItem;
+    private GUIScript gUI;
+    private SpriteRenderer render;
+    private Inventory inv;
+    private PlayerClickMove player;
+    private InventoryBox box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gUI = FindObjectOfType<GUIScript>();
+        render = GetComponent<SpriteRenderer>();
+        inv = FindObjectOfType<Inventory>();
+        player = FindObjectOfType<PlayerClickMove>();
+        box = FindObjectOfType<InventoryBox>();
+        currentItem = box.GetFreeItem(indexItem);
+        render.sprite = currentItem.itemElement.image;
+        if (itemReplace == currentItem.itemElement)
+        {
+            isReplace = true;
+        }
+    }
+
+    public void ShowText()
+    {
+        if (currentItem != null)
+        {
+            if (currentItem.itemElement != null && currentItem.amountItem > 0) gUI.AddText("Item: " + currentItem.itemElement.nameItem);
+        }
+    }
+
+    public void Use()
+    {
+        if (isReplace)
+        {
+            gUI.AddText("The item has been");
+            gUI.AddText("changed!");
+        }
+        else
+        {
+            Slot _item = inv.FindItem(itemReplace.idItem);
+            if(_item == null)
+            {
+                gUI.AddText("You do not have:");
+                gUI.AddText(itemReplace.nameItem);
+            }
+            else
+            {
+                if (player.ObjIsNearPlayer(transform.position, 1.1f))
+                {
+                    inv.RemoveAllUni(_item);
+                    inv.Add(currentItem);
+                    currentItem.itemElement = itemReplace;
+                    isReplace = true;
+                    render.sprite = itemReplace.image;
+                }
+                else
+                {
+                    gUI.AddText(currentItem.itemElement.nameItem + "is too far!");
+                }
+            }
+        }
+
+    }
+}
