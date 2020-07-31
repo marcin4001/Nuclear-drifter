5e86a20e343ab777688906e39537c94a7ff5e0a3
marcin4001 <mjachymczyk1@gmail.com>
2020-07-31 17:18:19 +0200

    Adding Gary and Joshua to Watown.

---
+++ Nuclear-drifter/Assets/Scripts/Missions/MissionDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionDoor : MonoBehaviour
+{
+    public Carpet door;
+    public int idMission = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (door == null) Destroy(gameObject);
+        MissionObj mission = MissionList.global.GetMission(idMission);
+        if (mission != null)
+        {
+            if (!mission.complete)
+            {
+                door.isLock = true;
+            }
+            else
+            {
+                door.isLock = false;
+            }
+        }
+        Destroy(gameObject);
+
+    }
+
+}
