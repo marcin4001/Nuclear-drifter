eb43616de5a47f44c9b4510f8de9755686364eaf
marcin4001 <mjachymczyk1@gmail.com>
2020-08-29 21:50:45 +0200

    Trigger Passport mission

---
+++ Nuclear-drifter/Assets/Scripts/Triggers/MissionItemTrig.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionItemTrig : MonoBehaviour
+{
+    public int idMission;
+    public Item item;
+    private Inventory inv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MissionObj mission = MissionList.global.GetMission(idMission);
+        if(mission != null)
+        {
+            if (mission.start) Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        if (item == null) Destroy(gameObject);
+        inv = FindObjectOfType<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(inv.FindItemB(item.idItem) && collision.tag == "Hero")
+        {
+            MissionObj mission = MissionList.global.GetMission(idMission);
+            if (mission != null)
+            {
+                mission.start = true;
+            }
+        }
+        Destroy(gameObject);
+    }
+
+}
