8275c1a3d152838d54e6f6c2f695082d69358071
marcin4001 <mjachymczyk1@gmail.com>
2020-09-05 23:51:14 +0200

    GUI Trade System and showing offert

---
+++ Nuclear-drifter/Assets/Scripts/Trade/TradeSlot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TradeSlot : MonoBehaviour
+{
+    public Slot itemSlot;
+    public Text labelSlot;
+    public Image imgSlot;
+    private GUIScript gUI;
+    private Inventory inv;
+    private TypeScene typeSc;
+    private EqChestController eqChest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gUI = FindObjectOfType<GUIScript>();
+        inv = GetComponentInParent<Inventory>();
+        typeSc = FindObjectOfType<TypeScene>();
+        eqChest = FindObjectOfType<EqChestController>();
+    }
+
+    public void ClickSlot()
+    {
+       
+    }
+
+    public void SetSlotStart(Slot newSlot)
+    {
+        itemSlot = newSlot;
+        imgSlot.enabled = true;
+        imgSlot.sprite = itemSlot.itemElement.image;
+        labelSlot.text = itemSlot.GetAmount();
+    }
+
+
+    public void SetSlot(Slot newSlot)
+    {
+        itemSlot = newSlot;
+        imgSlot.enabled = true;
+        imgSlot.overrideSprite = itemSlot.itemElement.image;
+        labelSlot.text = itemSlot.GetAmount();
+    }
+
+    public void ClearSlot()
+    {
+        itemSlot = null;
+        imgSlot.enabled = false;
+        labelSlot.text = "";
+    }
+}
---
+++ Nuclear-drifter/Assets/Scripts/Trade/TradeSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TradeSystem : MonoBehaviour
+{
+    public List<Slot> slots;
+    public TradeSlot[] tradeSlots;
+    private GUIScript gUI;
+    private PlayerClickMove move;
+    private TypeScene typeSc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gUI = FindObjectOfType<GUIScript>();
+        move = FindObjectOfType<PlayerClickMove>();
+        typeSc = FindObjectOfType<TypeScene>();
+        if (slots == null) slots = new List<Slot>();
+        else
+        {
+            if (slots.Count > 0)
+            {
+                for (int i = 0; i < tradeSlots.Length; i++)
+                {
+                    if (i < slots.Count)
+                    {
+                        tradeSlots[i].SetSlotStart(slots[i]);
+                    }
+                    else
+                    {
+                        tradeSlots[i].ClearSlot();
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
