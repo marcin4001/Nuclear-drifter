e8ba6ce6f8ab998b2b7b53b7d6e5987db042c00d
marcin4001 <marcinjachymczyk96@gmail.com>
2020-11-27 00:01:12 +0100

    The beginning of the subway mission

--- Nuclear-drifter/Assets/Scripts/Missions/MissionStart.cs
+++ Nuclear-drifter/Assets/Scripts/Missions/MissionStart.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class MissionStart : MonoBehaviour
 {
     public List<MissionInit> inits;
+    public List<MissionItem> missionItems;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (inits == null) inits = new List<MissionInit>();
+        if (missionItems == null) missionItems = new List<MissionItem>();
+        else
+        {
+            Invoke("DeactivationItem", 0.1f);
+        }
+    }
+
+    private void DeactivationItem()
+    {
+        foreach (MissionItem item in missionItems)
+        {
+            MissionObj mission = MissionList.global.GetMission(item.missionid);
+            if (mission != null)
+            {
+                if (!mission.start && item.item != null)
+                    item.item.SetActive(false);
+            }
+        }
     }
 
     public void Init(int id)
@@ -17,6 +36,11 @@ public class MissionStart : MonoBehaviour
         if(init != null)
         {
             init.box.LockOpen();
+        }
+        MissionItem item = missionItems.Find(x => x.missionid == id);
+        if(item != null)
+        {
+            if(item.item != null)item.item.SetActive(true);
         }
     }
 }
