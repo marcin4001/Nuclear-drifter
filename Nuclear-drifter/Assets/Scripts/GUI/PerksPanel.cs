1dd391b6aa631f968104ec4b68bad265490e3ebf
marcin4001 <marcinjachymczyk96@gmail.com>
2020-12-29 23:25:02 +0100

    Displaying the description of the perks

--- Nuclear-drifter/Assets/Scripts/GUI/PerksPanel.cs
+++ Nuclear-drifter/Assets/Scripts/GUI/PerksPanel.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 
 public class PerksPanel : MonoBehaviour
 {
+    public Text consoleDesc;
+    public PerkElement currentPerk;
+
     private Canvas perksCanvas;
     private GUIScript gUI;
     private TypeScene typeSc;
     private MapControl map;
     private FadePanel fade;
+    private Experience exp;
     private bool active = false;
 
     // Start is called before the first frame update
@@ -20,6 +24,7 @@ public class PerksPanel : MonoBehaviour
         map = FindObjectOfType<MapControl>();
         fade = FindObjectOfType<FadePanel>();
         perksCanvas = GetComponent<Canvas>();
+        exp = FindObjectOfType<Experience>();
         perksCanvas.enabled = false;
     }
 
@@ -35,10 +40,25 @@ public class PerksPanel : MonoBehaviour
         if (active)
         {
             Time.timeScale = 0.0f;
+            consoleDesc.text = "Welcome to the perks system!\n";
+            consoleDesc.text += "Your point: " + exp.lvlPoint + " LP\n";
         }
         else
         {
             Time.timeScale = 1.0f;
+            consoleDesc.text = "";
         }
+    }
+
+    public void ShowDesc(PerkElement perk)
+    {
+        consoleDesc.text = "";
+        string descText = "";
+        descText += perk.namePerk + "\n";
+        descText += perk.description + "\n";
+        descText += "Cost: " + perk.cost + " LP\n";
+        descText += "Your point: " + exp.lvlPoint + " LP\n";
+        consoleDesc.text = descText;
+        currentPerk = perk;
     }
 }
---
+++ Nuclear-drifter/Assets/Scripts/Player stats/PerkElement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerkElement : MonoBehaviour
+{
+    public Image lamp;
+    public Text levelCounter;
+
+    public string namePerk;
+    [TextArea(2,3)]
+    public string description;
+    public int indexPerk;
+    public int cost;
+
+    private PerksPanel panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = FindObjectOfType<PerksPanel>();
+    }
+
+    public void ClickBtn()
+    {
+        panel.ShowDesc(this);
+    }
+}
