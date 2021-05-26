dc2c18dcfaa2e4eb08035274b37c59b1807bffff
marcin4001 <marcinjachymczyk96@gmail.com>
2021-05-26 23:50:51 +0200

    Added TIP button in the Gun Shop

---
+++ Nuclear-drifter/Assets/Scripts/GUI/TipButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipButton : MonoBehaviour
+{
+    public string[] tips;
+    private GUIScript gUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gUI = FindObjectOfType<GUIScript>();
+    }
+
+    public void ClickTip()
+    {
+        if(tips.Length > 0)
+        {
+            gUI.ClearText();
+            foreach (string tip in tips)
+                gUI.AddText(tip);
+        }
+    }
+}
