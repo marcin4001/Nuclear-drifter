0828efebfaff4dabc6e683be250e0d36bbd71ca0
marcin4001 <marcinjachymczyk96@gmail.com>
2021-06-22 23:43:09 +0200

    Prologue scene 1

---
+++ Nuclear-drifter/Assets/Scripts/GUI/PrologueController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PrologueController : MonoBehaviour
+{
+    public int currentIndex = 0;
+    public PrologueObj[] prologueObjs;
+    public Text text;
+    public Image backgroundImg;
+    public Animator anim;
+    public bool activeEnter = true;
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundImg.overrideSprite = prologueObjs[currentIndex].backgroundImg;
+        text.text = prologueObjs[currentIndex].text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) && activeEnter)
+        {
+            StartCoroutine(ChangeBgImage());
+        }
+    }
+
+    private IEnumerator ChangeBgImage()
+    {
+        activeEnter = false;
+        anim.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1.5f);
+        currentIndex += 1;
+        if (currentIndex < prologueObjs.Length)
+        {
+            backgroundImg.overrideSprite = prologueObjs[currentIndex].backgroundImg;
+            text.text = prologueObjs[currentIndex].text;
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        anim.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1.5f);
+        //if (currentIndex < prologueObjs.Length)
+        //    text.text = prologueObjs[currentIndex].text;
+        activeEnter = true;
+    }
+}
---
+++ Nuclear-drifter/Assets/Scripts/GUI/PrologueObj.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PrologueObj 
+{
+    public Sprite backgroundImg;
+    [TextArea(2,4)]
+    public string text;
+}
