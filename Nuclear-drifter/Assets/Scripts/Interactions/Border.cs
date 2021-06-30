fcde36440cd604f88ad7cd899b59d430dbf47088
marcin4001 <marcinjachymczyk96@gmail.com>
2021-06-30 18:13:57 +0200

    Corrected dialog text and textures 0.7

--- Nuclear-drifter/Assets/Scripts/Interactions/Border.cs
+++ Nuclear-drifter/Assets/Scripts/Interactions/Border.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Border : MonoBehaviour
 {
     public int idMission = 0;
+    public GameObject gateOpen;
+    public GameObject gateClose;
     // Start is called before the first frame update
     void Start()
     {
         MissionObj mission = MissionList.global.GetMission(idMission);
         if (mission == null) Destroy(this);
-        if (mission.complete) Destroy(gameObject);
+        if (mission.complete)
+        {
+            Destroy(gameObject);
+            Destroy(gateClose);
+        }
+        else
+        {
+            Destroy(gateOpen);
+        }
     }
 }
