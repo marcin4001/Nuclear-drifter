a27c7b76fc7d9e20b8d26b46f211497eff8a3188
marcin4001 <marcinjachymczyk96@gmail.com>
2021-10-26 12:19:00 +0200

    Change of dialogues related to the mission "Bring $500 or Barry's machete for a passport"

--- Nuclear-drifter/Assets/Scripts/Interactions/BarrelController.cs
+++ Nuclear-drifter/Assets/Scripts/Interactions/BarrelController.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class BarrelController : MonoBehaviour
 {
+    public int idMission = 13;
     public Slot itemSlot;
     public Barrel[] barrels;
     // Start is called before the first frame update
     void Start()
     {
+        MissionObj mission = MissionList.global.GetMission(idMission);
+        if(mission != null)
+        {
+            if(!mission.start && !mission.complete)
+            {
+                foreach (Barrel barrel in barrels)
+                    barrel.gameObject.SetActive(false);
+                return;
+            }
+        }
+        else
+        {
+            foreach (Barrel barrel in barrels)
+                barrel.gameObject.SetActive(false);
+            return;
+        }
+
         foreach (Barrel barrel in barrels)
             barrel.isEmpty = true;
 
