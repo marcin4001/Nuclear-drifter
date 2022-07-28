57155cbb4b6cbe1c20e032196284318d21e99d0a
marcin4001 <marcinjachymczyk96@gmail.com>
2022-07-28 23:41:18 +0200

    Addition of Doctor Marlon's mission

---
+++ Nuclear-drifter/Assets/Scripts/Map/MineStartTextMap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineStartTextMap : MonoBehaviour
+{
+    public int idMission = 31;
+    public GameObject textMine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MissionObj obj = MissionList.global.GetMission(idMission);
+        if (obj == null)
+        {
+            textMine.SetActive(false);
+            return;
+        }
+        if (!obj.start)
+            textMine.SetActive(false);
+
+    }
+}
