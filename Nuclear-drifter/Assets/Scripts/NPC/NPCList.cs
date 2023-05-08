f6697fd1c13199a0625711dbfb335abb22ed42a6
marcin4001 <marcinjachymczyk96@gmail.com>
2023-05-08 23:02:13 +0200

    Added the Corn Prince's house to the Far Territories map

--- Nuclear-drifter/Assets/Scripts/NPC/NPCList.cs
+++ Nuclear-drifter/Assets/Scripts/NPC/NPCList.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class NPCList : MonoBehaviour
 {
@@ -12,11 +13,29 @@ public class NPCList : MonoBehaviour
 
     public NPCElement GetNPC(int index)
     {
+        if(index >= nPCs.Length)
+        {
+            List<NPCElement> list = nPCs.ToList();
+            for (int i = list.Count; i <= index; i++)
+            {
+                list.Add(new NPCElement());
+            }
+            nPCs = list.ToArray();
+        }
         return nPCs[index];
     }
 
     public void SetNPC(int index, bool _init, int _startIndex)
     {
+        if (index >= nPCs.Length)
+        {
+            List<NPCElement> list = nPCs.ToList();
+            for (int i = list.Count; i <= index; i++)
+            {
+                list.Add(new NPCElement());
+            }
+            nPCs = list.ToArray();
+        }
         nPCs[index].init = _init;
         nPCs[index].startIndex = _startIndex;
         if (nPCs[index].indexGlobal >= 0)
