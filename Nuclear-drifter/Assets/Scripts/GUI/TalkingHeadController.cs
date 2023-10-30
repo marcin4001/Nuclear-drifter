bba010a120716f5b5419e83c05719899e0226071
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2023-10-30 21:27:00 +0100

    Changing the appearance of a marker on the map indicating an undiscovered location.

--- Nuclear-drifter/Assets/Scripts/GUI/TalkingHeadController.cs
+++ Nuclear-drifter/Assets/Scripts/GUI/TalkingHeadController.cs
@@ -18,6 +18,9 @@ public class TalkingHeadController : MonoBehaviour
     private float[] spectrumData;
     private int pitch = 0;
     private bool switchOn = true;
+
+    private float mouthUpdateTimer = 0f;
+    private float mouthUpdateInterval = 0.03f;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +31,8 @@ public class TalkingHeadController : MonoBehaviour
     void Update()
     {
         if(headObj == null) return;
+
+        mouthUpdateTimer += Time.deltaTime;
         if (!source.isPlaying)
         {
             if(isComplete)
@@ -54,16 +59,21 @@ public class TalkingHeadController : MonoBehaviour
         foreach(float spectrum in spectrumData)
             averageSpectrum += spectrum;
         averageSpectrum = averageSpectrum/spectrumData.Length;
-        int indexMouth = Mathf.FloorToInt(averageSpectrum * (headObj.mouth.Length - 1 - pitch) * 1000);
-        Debug.Log(indexMouth);
-        if (indexMouth < headObj.mouth.Length)
+        if (mouthUpdateTimer >= mouthUpdateInterval)
         {
-            mouthImg.overrideSprite = headObj.mouth[indexMouth];
+            int indexMouth = Mathf.FloorToInt(averageSpectrum * (headObj.mouth.Length - 1 - pitch) * 1000);
+            Debug.Log(indexMouth);
+            if (indexMouth < headObj.mouth.Length)
+            {
+                mouthImg.overrideSprite = headObj.mouth[indexMouth];
+            }
+            else
+            {
+                mouthImg.overrideSprite = headObj.mouth[headObj.mouth.Length - 1];
+            }
+            mouthUpdateTimer = 0f;
         }
-        else
-        {
-            mouthImg.overrideSprite = headObj.mouth[headObj.mouth.Length - 1];
-        }
+        
     }
 
     public void SetHead(NPCBasic _NPC)
@@ -81,6 +91,7 @@ public class TalkingHeadController : MonoBehaviour
         }
         backgroundImg.SetActive(true);
         mouthImg.overrideSprite = headObj.mouth[0];
+        mouthUpdateTimer = 1f;
     }
 
     public void Close()
@@ -99,6 +110,7 @@ public class TalkingHeadController : MonoBehaviour
         pitch = _pitch;
         source.Play();
         isComplete = true;
+        mouthUpdateTimer = 1f;
     }
 
     private void StartBlink()
